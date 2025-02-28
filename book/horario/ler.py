54c642710edabb7383540b2aa281384d12ff9c5f
Thomas <thomasadsa@gmail.com>
2025-02-28 19:17:45 -0300

    POO
    Programação Orientada a Objeto

--- book/horario/ler.py
+++ book/horario/ler.py
@@ -1,53 +1,78 @@
 from openpyxl import load_workbook
 
-arq = load_workbook('HorarioB.xlsx')
-cur = arq['Plan1']
+class Horario:
+    def __init__(self):
+        self.seg = []
+        self.ter = []
+        self.qua = []
+        self.qui = []
+        self.sex = []
+        self.cur = None
+        self.arq = load_workbook('Horario.xlsx')
 
-ter = []
-qua = []
-qui = []
-seg = []
-sex = []
+    def esc(self):
+        escolha = int(input("1. A\n 2. B"))
+        if escolha == 1:
+            self.cur = self.arq['A']
 
-for i in range(5):
-    cont=0
-    day = None
-    for j in cur.rows:
-        if cont <= 0:
-            cont += 1
-            continue
-        if not j[i].value:
-            continue
-        
-        
-        
+        elif escolha == 2:
+            self.cur = self.arq['B']
+
+        else:
+            print("Erro! COloque um valor válido")
+    
+
+    def dia(self, i, j, day):
         match day:
             case 'SEG':
                 if not day:
                     day = 'SEG'
-                seg.append(j[i].value)
+                self.seg.append(j[i].value)
 
             case 'TER':
                 if not day:
                     day = 'TER'
-                ter.append(j[i].value)
+                self.ter.append(j[i].value)
 
             case 'QUA':
                 if not day:
                     day = 'QUA'
-                qua.append(j[i].value)
+                self.qua.append(j[i].value)
 
             case 'QUI':
                 if not day:
                     day = 'QUI'
-                qui.append(j[i].value)
+                self.qui.append(j[i].value)
 
             case 'SEX':
                 if not day:
                     day = 'SEX'
-                sex.append(j[i].value)
+                self.sex.append(j[i].value)
             
             case _:
                 day = j[i].value
-    
-    print(seg, ter, qua, qui, sex)
+
+        return day
+            
+
+    def main(self):
+        self.esc()
+
+        for i in range(5):
+            cont=0
+            day = None
+            for j in self.cur.rows:
+                if cont <= 0:
+                    cont += 1
+                    continue
+                if not j[i].value:
+                    continue
+
+                day = self.dia(i, j, day)
+        
+        print(self.seg, self.ter, self.qua, self.qui, self.sex)
+                
+        
+        
+sla = Horario()
+sla.main()
