a85b3687682b1aa13403eeca5f3caf38f470beb0
Thomas <thomasadsa@gmail.com>
2025-03-02 10:49:41 -0300

    Dia por extenso
    Adicionei a função datetime mas não sei se vou deixar em um arquivo só, também estou adicionando a função de pegar toda a lista das matérias diárias

--- book/horario/ler.py
+++ book/horario/ler.py
@@ -1,4 +1,5 @@
 from openpyxl import load_workbook
+from datetime import date
 
 class Horario:
     def __init__(self):
@@ -9,6 +10,9 @@ class Horario:
         self.sex = []
         self.cur = None
         self.arq = load_workbook('Horario.xlsx')
+        self.week_day = date.today()
+        self.us = None
+        self.extension_day = ''
 
     def esc(self):
         escolha = int(input("1. A\n 2. B"))
@@ -21,7 +25,6 @@ class Horario:
         else:
             print("Erro! COloque um valor válido")
     
-
     def dia(self, i, j, day):
         match day:
             case 'SEG':
@@ -55,7 +58,7 @@ class Horario:
         return day
             
 
-    def main(self):
+    def apd(self):
         self.esc()
 
         for i in range(5):
@@ -69,10 +72,48 @@ class Horario:
                     continue
 
                 day = self.dia(i, j, day)
+    
+    def week_day_extension(self) -> str:
+        day = self.week_day.isoweekday()
+        extension_day = ''
+        if not isinstance(day, int):
+            raise ValueError('O valor deve ser INT!')
+
+        match day:
+            case 1:
+                self.extension_day += "segunda"
+                self.us = self.seg
+
+            case 2:
+                self.extension_day += 'terça'
+                self.us = self.ter
+
+            case 3:
+                self.extension_day += 'quarta'
+                self.us = self.qua
+
+            case 4:
+                self.extension_day += 'quinta'
+                self.us = self.qui
+
+            case 5:
+                self.extension_day += 'sexta'
+                self.us = self.sex
+
+            case 6:
+                self.extension_day += 'sábado'
+                self.us = 'None2'
+
+            case 7:
+                self.extension_day += 'domingo'
+                self.us = 'Nonse'
+
+            case _:
+                self.extension_day = None
         
-        print(self.seg, self.ter, self.qua, self.qui, self.sex)
-                
-        
-        
-sla = Horario()
-sla.main()
+
+    
+dia = Horario()
+dia.apd()
+dia.week_day_extension()
+print(dia.us, dia.extension_day)
