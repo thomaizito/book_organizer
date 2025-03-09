fcbdb5adda35aab3e46d6109a0f9d2f0f26d3d9f
Thomas <thomasadsa@gmail.com>
2025-03-09 16:45:31 -0300

    resolvendo algumas coisas

--- book/book.py
+++ book/book.py
@@ -4,75 +4,39 @@ from book.arquivo.read import Read
 class OrgLiv:
     def __init__(self):
         self.livros = {}
-        self.cn_flag = ['fisica',
-                   'quimica',
-                   'biologia']
-        
-        self.ch_flag = ['sociologia',
-                   'geografia',
-                   'filosofia',
-                   'historia']
-        
-        self.m_flag = ['matematica']
-
-        self.l_flag = ['portugues',
-                  'educacao fisica',
-                  'ingles',
-                  'literatura']
-        
         self.cn = []
         self.ch = []
         self.m = []
         self.l = []
-        
         self.dia = Horario()
         self.memory = Read('./book/arquivo/livros.txt')
     
+
     def horario_day(self):
         self.dia.today_day
         
         print(self.dia.extenso)
 
-
-
     # Limpa de fato os itens
-    def limp(self, it:dict):
+    @staticmethod
+    def limp(it:list):
         it_ret = it
         flag = set()
-        print(it_ret)
 
+<<<<<<< HEAD
         for lista in it.items():
             if lista[1] == ['']:
                 continue
 
             self.verification_ordenation(lista)
-
-    def verification_ordenation(self, name:tuple):
-        if name[0] in self.cn_flag:
-            if name[1] not in self.cn:
-                self.cn.append(name[1])
-            else:
-                return
-
-        elif name[0] in self.ch_flag:
-            if name[1] not in self.ch:
-                self.ch.append(name[1])
-            else:
-                return 
-
-        elif name[0] in self.m_flag:
-            if name[1] not in self.m:
-                self.m.append(name[1])
-            else:
-                return
-
-        elif name[0] in self.l_flag:
-            if name[1] not in self.l:
-                self.l.append(name[1])
-            else:
-                return 
+=======
+        for lista in it:
+            for inner in lista:
+                flag.add(tuple(inner))
         
-
+        it_ret = list(flag)
+        return it_ret
+>>>>>>> parent of db17f0d (Funcionou mais ou menos)
 
     # Deixa de forma apresentável
     @staticmethod 
@@ -95,8 +59,34 @@ class OrgLiv:
         if livros:
             self.livros = self.limp(livros)
         
+    def all_books_apd(self, items):
+                
+        self.cn = [
+            items['fisica'],
+            items['quimica'],
+            items['biologia']
+        ]
+
+        self.ch = [
+            items['sociologia'],
+            items['geografia'],
+            items['filosofia'],
+            items['historia']
+        ]       
+
+        self.m = [
+            items['matematica']
+        ]
+
+        self.l = [
+            items['portugues'],
+            items['ed'],
+            items['ingles'],
+            items['literatura']
+        ]
 
     def display(self):
+<<<<<<< HEAD
         
         print(self.cn,
               self.ch,
@@ -115,3 +105,7 @@ class OrgLiv:
 sla = OrgLiv()
 
 sla.memory_books()
+=======
+        print(self.livros)
+        return self.livros
+>>>>>>> parent of db17f0d (Funcionou mais ou menos)
